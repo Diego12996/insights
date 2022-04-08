625be8532b0c269391c14cc99c46223e9a33b5d1
Diego Manrique <diego.manrique.m@uni.pe>
2022-04-08 15:30:04 -0500

    Create case method

--- restaurant-insights.rb
+++ restaurant-insights.rb
@@ -8,7 +8,24 @@ class InsightsApp
   def start
     puts "Welcome to the Restaurants Insights!"
     puts "Write 'menu' at any moment to print the menu again and 'quit' to exit."
+    menu
     print "> "
+    option, param = gets.chomp.split
+
+    case option
+    when "1" then "execute 1"
+    when "2" then "execute 2"
+    when "3" then "execute 3"
+    when "4" then "execute 4"
+    when "5" then "execute 5"
+    when "6" then "execute 6"
+    when "7" then "execute 7"
+    when "8" then "execute 8"
+    when "9" then "execute 9"
+    when "quit" then "quit"
+    when "menu" then menu        
+    end
+
   end
 
   def options
@@ -27,6 +44,7 @@ class InsightsApp
     puts "9. The list of dishes and the restaurant where you can find it at a lower price."
     puts "10. The favorite dish for [age=number | gender=string | occupation=string | nationality=string]"
     puts "---"
+    puts "Pick a number from the list and an [option] if necessary"
   end
 end
 

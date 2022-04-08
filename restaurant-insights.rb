469bd2875a84d3a5e7cfe3ef4f3ac889e64f71c3
Diego Manrique <diego.manrique.m@uni.pe>
2022-04-08 16:12:18 -0500

    create methods

--- restaurant-insights.rb
+++ restaurant-insights.rb
@@ -13,16 +13,17 @@ class InsightsApp
     option, param = gets.chomp.split
 
     case option
-    when "1" then "execute 1"
-    when "2" then "execute 2"
-    when "3" then "execute 3"
-    when "4" then "execute 4"
-    when "5" then "execute 5"
-    when "6" then "execute 6"
-    when "7" then "execute 7"
-    when "8" then "execute 8"
-    when "9" then "execute 9"
-    when "quit" then "quit"
+    when "1" then search_by
+    when "2" then unique_dish
+    when "3" then users_by
+    when "4" then top10_by_visitors
+    when "5" then top10_by_sales
+    when "6" then top10_by_average_expense
+    when "7" then average_expense_by
+    when "8" then sales_per_month
+    when "9" then best_price_dish
+    when "10" then favorite_dish_by
+    when "quit" then print "Good bye!"
     when "menu" then menu        
     end
 
@@ -46,6 +47,38 @@ class InsightsApp
     puts "---"
     puts "Pick a number from the list and an [option] if necessary"
   end
+
+  def search_by
+  end
+
+  def unique_dish
+  end
+
+  def users_by
+  end
+
+  def top10_by_visitors
+  end
+
+  def top10_by_sales
+  end
+
+  def top10_by_average_expense
+  end
+
+  def average_expense_by
+  end
+
+  def sales_per_month
+  end
+
+  def best_price_dish
+  end
+
+  def favorite_dish_by
+  end
+
+
 end
 
 app = InsightsApp.new
